fd5b1c226081a10dc164c5912cc2d2926feec386
saitou-cpi <odeco_de_odeco@yahoo.co.jp>
2024-07-27 14:29:07 +0900

    blue.serviceにenvを追加

--- blue/run.sh
+++ blue/run.sh
@@ -3,9 +3,11 @@
 export FLASK_ENV=production
 
 if [ "$1" == "blue" ]; then
-    gunicorn -w 4 -b 127.0.0.1:8000 app:app
-elif [ "$1" == "green" ]; thrn
-    gunicorn -w 4 -b 127.0.0.1:8001 app:app
+    cd /home/ec2-user/BlueGreenTest/blue
+    /home/ec2-user/.venv/bin/gunicorn -w 4 -b 127.0.0.1:8000 app:app
+elif [ "$1" == "green" ]; then
+    cd /home/ec2-user/BlueGreenTest/green
+    /home/ec2-user/.venv/bin/gunicorn -w 4 -b 127.0.0.1:8001 app:app
 else
-    echo "Usage: ./run.sh [blue/green]"
+    echo "Usage: ./run.sh [blue|green]"
 fi
