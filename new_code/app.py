4241499cee7fb06416ae51098bc6f03ef66d883a
saitou-cpi <115441742+saitou-cpi@users.noreply.github.com>
2024-08-03 16:30:55 +0900

    Update app.py

--- new_code/app.py
+++ new_code/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask with Gunicorn and Nginx!!!!"
+    return "Hello, Flask with Gunicorn and Nginx!"
 
 if __name__ == "__main__":
     app.run()
